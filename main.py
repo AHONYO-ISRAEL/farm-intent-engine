4e45d0dcee235f4c84d7f471ea11918b4c4ca9b7
AHONYO-ISRAEL <izy.clapp@gmail.com>
2026-02-24 13:04:32 +0000

    Added simple security with api key check

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
-
 from app.intent_egine import IntentEngine
 from app.models import Query
+from fastapi.middleware.cors import CORSMiddleware
+
+from middleware.auth import verify_api_key
+
+origins = [
+    "https://your-frontend.com",
+    "http://localhost:3000"
+]
+
+
+
 
 app = FastAPI()
 engine = IntentEngine()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
@@ -14,5 +31,5 @@ def root():
 
 
 @app.post("/predict")
-def predict(query: Query):
+def predict(query: Query, token_data=Depends(verify_api_key)):
     return engine.detect_intent(query.text)
